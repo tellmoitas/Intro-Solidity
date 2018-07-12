50fe174c3810e570e764a199e036c2e939e89610
Tell Marcus de Souza Moitas <tellmoitas@gmail.com>
2018-07-11 21:39:59 -0300

    Create variaveis.sol

---
+++ variaveis.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.7;
+
+contract Variaveis {
+    string um = 'um';
+    string public dois = 'dois';
+    int public tres = 3;
+    int public negativo = -3;
+    uint public quatro = 4; //unit nao pode ser negativo
+    address meuEndereco = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+}
